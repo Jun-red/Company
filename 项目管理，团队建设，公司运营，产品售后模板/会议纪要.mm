c4d262ec2054e4c2424f226c40e1c281061bc318
Jun-red <1228407448@qq.com>
2024-05-20 13:53:25 +0800

    整理模板

---
+++ 项目管理，团队建设，公司运营，产品售后模板/会议纪要.mm
@@ -0,0 +1,27 @@
+
+<map>
+  <node ID="root" TEXT="会议纪要">
+    <node TEXT="💡 言必当理，事必当务。—— 荀况" ID="3244b952c0b5d7e680d8f6769df84268" STYLE="bubble" POSITION="right"/>
+    <node TEXT="基本信息" ID="441d50a003bf0fcea44761c8ced7567e" STYLE="bubble" POSITION="right">
+      <node TEXT="会议时间" ID="ea738014623f09bb1814a5ef6e11697e" STYLE="fork"/>
+      <node TEXT="会议地点" ID="08a608011701785d9bed5705b0a3f2a2" STYLE="fork"/>
+      <node TEXT="参会人" ID="e0943bc6b9eae62372683f71bfcbda3f" STYLE="fork"/>
+      <node TEXT="相关文档" ID="0541b6c7d5bb934213677e942a7ee6bb" STYLE="fork"/>
+    </node>
+    <node TEXT="议题 1：输入具体议题 " ID="c824de36ce40e9de4d641ea2a3727126" STYLE="bubble" POSITION="right">
+      <node TEXT="发言人：@ 提及发言人" ID="ba397beea86a456bbe1a443c23eed27a" STYLE="fork"/>
+      <node TEXT="会后相关行动" ID="82c99837e26b79a97f5fa1d26a751212" STYLE="fork">
+        <node TEXT="行动 1" ID="1d280098129d29a38e7b27c160515fbd" STYLE="fork"/>
+        <node TEXT="行动 2 " ID="54571c57bdfb8152c04599716d9f0d0e" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="议题 2：输入具体议题" ID="f25c21c5fc58c3a15c98d5b86122bdaa" STYLE="bubble" POSITION="right">
+      <node TEXT="发言人：@ 提及发言人" ID="383daf513a956a784e86f3161eb86a05" STYLE="fork"/>
+      <node TEXT="会后相关行动" ID="810ac20f4941bffa76b880231398c546" STYLE="fork">
+        <node TEXT="行动 1" ID="daa3a6105c5c434f8238ac7f189d9941" STYLE="fork"/>
+        <node TEXT="行动 2" ID="82e4df5bf203f6711bdbc674ee53e80a" STYLE="fork"/>
+      </node>
+    </node>
+    <node TEXT="其他事项" ID="1f122a77f431ab939c537ce43d3ba7ae" STYLE="bubble" POSITION="right"/>
+  </node>
+</map>
---
+++ 项目管理，团队建设，公司运营，产品售后模板/项目规划.mm
@@ -0,0 +1,23 @@
+
+<map>
+  <node ID="root" TEXT="项目规划">
+    <node TEXT="项目背景" ID="ae1361aaba76c5103c4de566c1e72786" STYLE="bubble" POSITION="right">
+      <node TEXT="介绍项目成立的背景，输入 @ 插入相关云文档 " ID="749e41b7e0bd636d8f50669d65a7008f" STYLE="fork"/>
+      <node TEXT="......" ID="58362b4d08d6b9ba312106a6b6a63247" STYLE="fork"/>
+    </node>
+    <node TEXT="项目目标" ID="f7feb30a9573b2f0542c1b0ec7ad9474" STYLE="bubble" POSITION="right">
+      <node TEXT="目标 1：具体、细化、可拆解" ID="7b2c5e8ddf574ecc1e74e5d6954bb9b7" STYLE="fork"/>
+      <node TEXT="目标 2：......" ID="7a855fecee1e72cf1d8394e42a5c9525" STYLE="fork"/>
+    </node>
+    <node TEXT="项目成员" ID="167e8015888895ffd6bf43711b03ed1e" STYLE="bubble" POSITION="right">
+      <node TEXT="项目负责人：输入“@+人名”提及相关人员" ID="a32eeba173a4d5f90a54d43abfd4e33f" STYLE="fork"/>
+      <node TEXT="设计负责人：......" ID="b514310c51f49bc8091a9bce68c45e57" STYLE="fork"/>
+      <node TEXT="开发负责人：......" ID="7054bda9e8b88d9b7d53303874925430" STYLE="fork"/>
+      <node TEXT="......" ID="ab9fa964dd09d41a157726bef6c5e40e" STYLE="fork"/>
+    </node>
+    <node TEXT="项目拆解与分工" ID="f5b9a6b8676fb3cc8fec8707924870bf" STYLE="bubble" POSITION="right">
+      <node TEXT="任务 1：描述任务内容，输入“@+人名”提及相关人员" ID="9f84499cae94bd3176a3f3861ef2ea08" STYLE="fork"/>
+      <node TEXT="任务 2：......" ID="0f37de975907891902987e9a0699dad2" STYLE="fork"/>
+    </node>
+  </node>
+</map>
